b1a5ad2c603c0709e3c9807c512d4b9ec67b0d92
Colby Young <cyoung38@huskers.unl.edu>
2019-08-29 12:40:48 -0500

    Create hello.c
    prints name and major

---
+++ hello.c
@@ -0,0 +1,15 @@
+/*
+Name: Colby Young
+Date: 8/27/2019
+Class: CSCE-155E
+Activity: Lab 1
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  printf("Colby Young\n");
+  printf("Computer Engineering\n");
+  return 0;
+}
